50a62ad23f853b226b0c6351e2c0782b4927ab56
Xuanting_Liu <stayinwind@sjtu.edu.cn>
2026-05-05 13:03:50 +0800

    update dribbling parameters

--- Core/Src/action.c
+++ Core/Src/action.c
@@ -90,7 +90,7 @@ void do_dribbler( int dribbler )
 	#if (DIRB_MOTOR_VERSION == OLD_DIRB_MOTOR)
 		gain = 0.5;
 	#elif (DIRB_MOTOR_VERSION == NEW_DIRB_MOTOR)
-		gain = 0.1; 
+		gain = 0.55; 
 	#endif	
 
     if(dribbler_temp == 0)
